ffa3360c4e28c67f4ae964f75ea804261295969e
Aniya Modi <aniyamodi@Aniyas-MacBook-Air.local>
2024-06-14 15:50:01 +0530

    First Commit

--- 19.py
+++ 19.py
@@ -2,3 +2,4 @@ import string
 string_with_punctuation = input("Enter a string with punctuation: ")
 cleaned_string = string_with_punctuation.translate(str.maketrans("", "", string.punctuation))
 print("String without punctuation:", cleaned_string)
+
--- 2.py
+++ 2.py
@@ -1,9 +1,5 @@
 # Program to check if a number is even or odd
-
-# Taking input from user
 num = int(input("Enter a number: "))
-
-# Checking if the number is even or odd
 if num % 2 == 0:
     print(num, "is even.")
 else:
